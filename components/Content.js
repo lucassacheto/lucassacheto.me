8e99c8987e0cbe9c7ebc8e823cbd0d329a261e9f
Lucas Sacheto <lcsacheto@gmail.com>
2023-12-01 15:53:04 -0500

    dev.to link added

--- components/Content.js
+++ components/Content.js
@@ -5,6 +5,7 @@ class Content extends HTMLElement {
             
             <a href="http://www.linkedin.com/in/lucassacheto"  target="_blank">LinkedIn</a>
             <a href="http://www.github.com/lucassacheto" target="_blank">GitHub</a>
+            <a href="https://dev.to/lucassacheto" target="_blank">Dev.to</a>
             <a href="http://www.x.com/eusouoluks" target="_blank">X</a>
             <a href="http://www.instagram.com/eusouoluks" target="_blank">Instagram</a>
             <p>@2023</p> 
